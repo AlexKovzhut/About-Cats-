011c00096b92768d8cf7fc8b2262e3026eaea146
Alexander Kovzhut <akovzhut@icloud.com>
2021-05-26 23:51:08 +0300

    Initial commit

--- LogonProject/ViewController.swift
+++ LogonProject/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var forgotLoginButton: UIButton!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destinationVC = segue.destination as? SecondViewController else { return }
+        destinationVC.login = loginTextField.text
+        }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func loginPressedButton(_ sender: UIButton) {
+        if loginTextField.text == "Admin" && passwordTextField.text == "Qwerty123" {
+        performSegue(withIdentifier: "loginSegue", sender: nil)
+        }
+    }
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        guard segue.identifier == "unwindSegue" else { return }
+    }
 
 }
 
